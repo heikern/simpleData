4bf180289f8433498f86647cf9027ca390b1c422
Hei Kern Leong <heikern@hotmail.com>
2015-11-10 13:33:36 +0800

    deleted Print Environment Variables

--- App/__init__.py
+++ App/__init__.py
@@ -6,7 +6,5 @@ import os
 dataApp = Flask(__name__)
 #dataApp.config.from_object('config.DevelopmentConfig')
 dataApp.config.from_object(os.environ['APP_SETTINGS'])
-print os.environ['APP_SETTINGS']
-print os.environ['DATABASE_URL']
 db = SQLAlchemy(dataApp)
 from App import views
