953f4a2ab89142679796d114e2bebc3beb819730
Hei Kern Leong <heikern@hotmail.com>
2015-11-10 15:39:07 +0800

    Config try 2

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class BaseConfig():
-	#SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
-	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+	#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	SECRET_KEY = '\x98k\xa4\xa4:\xedzO0\x8a\xc9X\x93\xc9\x94o*p\x9atum)\xb5'
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	DEBUG = False
