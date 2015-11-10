766362609311f9b8359099eab497832d1d66c47d
Hei Kern Leong <heikern@hotmail.com>
2015-11-10 15:35:45 +0800

    testing config

--- App/__init__.py
+++ App/__init__.py
@@ -4,6 +4,7 @@ import os
 
 
 dataApp = Flask(__name__)
-dataApp.config.from_object(os.environ['APP_SETTINGS'])
+#dataApp.config.from_object(os.environ['APP_SETTINGS'])
+dataApp.config.from_object('config.ProductionConfig')
 db = SQLAlchemy(dataApp)
 from App import views
